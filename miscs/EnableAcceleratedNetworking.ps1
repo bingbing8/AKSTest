75e3f1e7e38acbe5415626e9bf78655e3adaef16
Yanbing Wang <yawang@microsoft.com>
2021-06-14 22:07:02 -0700

    install two drivers

---
+++ miscs/EnableAcceleratedNetworking.ps1
@@ -0,0 +1,19 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $true)]    
+    [string] $resourcegroupName
+    )
+
+Write-Output "Stopping VM $($_.Name)..."
+$vms = Get-AzVM -ResourceGroupName $resourcegroupName
+$vms | Stop-azvm -force
+
+$nics = Get-AzNetworkInterface -ResourceGroupName $resourcegroupName
+$nics | Where-Object {$_.Name -notlike "*master*" } | ForEach-Object {
+    Write-Output "enabling Accelerated Networking on $($_.Name)"
+    $_.EnableAcceleratedNetworking = $true
+    $_ | Set-AzNetworkInterface
+}
+
+Write-Output "Starting VM $($_.Name)..."
+$vms | Start-azvm
--- miscs/acceleratedNetwork.ps1
+++ miscs/acceleratedNetwork.ps1
@@ -3,12 +3,18 @@ if(-Not (Test-path c:\test -PathType Container))
 {
     New-Item c:\test -ItemType Directory -force
 }
-$driverPath = "c:\test\MLNX_WinOF2-2_60_51000_All_x64.exe"
-if(-Not (Test-Path $driverPath -PathType Leaf))
+$cx3driverPath = "c:\test\MLNX_VPI_WinOF-5_50_54000_All_win2019_x64.exe"
+if(-Not (Test-Path $cx3driverPath -PathType Leaf))
 {
-    Invoke-WebRequest -Uri http://www.mellanox.com/downloads/WinOF/MLNX_WinOF2-2_60_51000_All_x64.exe -OutFile $driverPath
+    Invoke-WebRequest -Uri https://mywinimgbldstg.blob.core.windows.net/mywinimgbldcontr/MLNX_VPI_WinOF-5_50_54000_All_win2019_x64.exe -OutFile $cx3driverPath
 }
-start-process -Wait -FilePath $driverPath "/s","/v""/qn"""
+
+$cx4driverPath = "MLNX_WinOF2-2_60_51000_All_x64.exe"
+if(-Not (Test-Path $cx4driverPath -PathType Leaf))
+{
+    Invoke-WebRequest -Uri http://www.mellanox.com/downloads/WinOF/MLNX_WinOF2-2_60_51000_All_x64.exe -OutFile $cx4driverPath
+}
+start-process -Wait -FilePath $cx4driverPath "/s","/v""/qn"""
 
 
 if(-Not (Test-path c:\tools -PathType Container))
