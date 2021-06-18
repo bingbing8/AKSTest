6828aa4fa09c2e452c717c67c7993bdbc76b6db7
Yanbing Wang <yawang@microsoft.com>
2021-06-18 09:50:12 -0700

    add workaround for icm 245223040

--- miscs/acceleratedNetwork.ps1
+++ miscs/acceleratedNetwork.ps1
@@ -1,20 +1,22 @@
-﻿
+﻿Get-NetAdapterRsc | Disable-NetAdapterRsc
 if(-Not (Test-path c:\test -PathType Container))
 {
     New-Item c:\test -ItemType Directory -force
 }
-$cx3driverPath = "c:\test\MLNX_VPI_WinOF-5_50_54000_All_win2019_x64.exe"
+$cx3File = "MLNX_VPI_WinOF-5_50_54000_All_win2019_x64.exe"
+$cx3driverPath = "c:\test\$cx3File"
 if(-Not (Test-Path $cx3driverPath -PathType Leaf))
 {
-    Invoke-WebRequest -Uri https://mywinimgbldstg.blob.core.windows.net/mywinimgbldcontr/MLNX_VPI_WinOF-5_50_54000_All_win2019_x64.exe -OutFile $cx3driverPath
+    Invoke-WebRequest -Uri "https://mywinimgbldstg.blob.core.windows.net/mywinimgbldcontr/$cx3File" -OutFile $cx3driverPath
 }
 
 start-process -Wait -FilePath $cx3driverPath "/s","/v""/qn"""
 
-$cx4driverPath = "MLNX_WinOF2-2_60_51000_All_x64.exe"
+$cx4File = "MLNX_WinOF2-2_60_51000_All_x64.exe"
+$cx4driverPath = "c:\test\$cx4File"
 if(-Not (Test-Path $cx4driverPath -PathType Leaf))
 {
-    Invoke-WebRequest -Uri http://www.mellanox.com/downloads/WinOF/MLNX_WinOF2-2_60_51000_All_x64.exe -OutFile $cx4driverPath
+    Invoke-WebRequest -Uri "http://www.mellanox.com/downloads/WinOF/$cx4File" -OutFile $cx4driverPath
 }
 start-process -Wait -FilePath $cx4driverPath "/s","/v""/qn"""
 
